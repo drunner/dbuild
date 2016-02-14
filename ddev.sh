fa8f72b3ca9ea34447589e22fa8f8b0e9cafd77e
j842 <j@842.be>
2016-02-14 20:26:30 +1300

    Rewrite how we determine what to do with a dService.

---
+++ ddev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Configuration file for ddev.
+
+# Name of Docker Hub image we eventually build to (ignoring any branch info):
+BUILDNAME="drunner/ddev"
+
+# Whether or not we're a dService:
+DSERVICE="yes"
+
+# Non-empty to install and update dev checkout as this dRunner Servicename.
+DEVSERVICENAME="ddev"
+
